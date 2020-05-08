5cce0d5364eae5deef1deee21041a76f8a761025
Eduard Arango <eduard.arango@correounivalle.edu.co>
2020-05-07 22:32:10 -0500

    Added styles to title of ForecastExtended component

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import './styles.css'
 
 class ForecastExtended extends Component {
     render(){
         const {city} = this.props;
-        return(<div>Forecast Extended for {city}</div>);
+        return(
+        <div>
+            <h2 className='forecast-title'>Forecast Extended for {city}</h2>
+        </div>);
     }
 }
 
