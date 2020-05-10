a51146911641a4be94271d89b9a1f8a867e89892
Eduard Arango <eduard.arango@correounivalle.edu.co>
2020-05-09 20:02:05 -0500

    Added fetch in ForecastExtended component to get forecast of the city selected and added momentjs library to transform data

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,35 +1,59 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem';
+import transformForecast from './../services/transformForecast';
 import './styles.css'
 
-const days = [
-    'Lunes',
-    'Martes',
-    'Miercoles',
-    'Jueves',
-    'Viernes'
-]
 
-const data = {
-    temperature: 23,
-    humidity: 15,
-    weatherState: 'SUNNY',
-    wind: '12 m/s'
-}
+const url = 'http://api.openweathermap.org/data/2.5/forecast';
+const api_key = '82a4b5915dec0a2c66e26bdcb9af7faf';
 
 class ForecastExtended extends Component {
 
-    renderForecastItemDays() {
-        return days.map(day => <ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>);
+    constructor(){
+        super();
+        this.state = { forecastData : null}
+    }
+
+    componentDidMount() {
+        const url_forecast = `${url}?q=${this.props.city}&appid=${api_key}`;
+
+        fetch(url_forecast).then(
+            data => data.json()
+        ).then(
+            weather_data => {
+                console.log(weather_data);
+                const forecastData = transformForecast(weather_data);
+                console.log(forecastData);
+                this.setState({forecastData});
+            }
+        );
+    }
+    
+    renderForecastItemDays(forecastData) {
+        return forecastData.map(forecast => 
+            <ForecastItem 
+            key={`${forecast.weekDay}${forecast.hour}`}
+            weekDay={forecast.weekDay} 
+            hour={forecast.hour} 
+            data={forecast.data}></ForecastItem>
+        );
+    }
+
+    renderProgress(){
+        return <h3>Loading Forecast Extended...</h3>;
     }
 
     render(){
         const {city} = this.props;
+        const {forecastData} = this.state;
         return(
         <div>
             <h2 className='forecast-title'>Forecast Extended for {city}</h2>
-            {this.renderForecastItemDays()}
+            { forecastData ?
+                this.renderForecastItemDays(forecastData) :
+                this.renderProgress()
+            }
         </div>);
     }
 }
